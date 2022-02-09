4ad9d9ff2885f52c7ed380327e42ac2896d24100
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-02-09 11:41:37 -0800

    added pic and resized

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 export const cars = [{
     id: '1',
     brand:'toyota',
-    model:['Corolla'],
+    model:['Corolla', 'sedan'],
     image: 'corolla',
     
 },
 {
     id: '2',
     brand:'subaru',
-    model:['Impreza'],
+    model:['Impreza', 'offroad'],
     image: 'impreza',
 },
 {
     id: '3', 
     brand:'ford',
-    model:['Mustang'],
+    model:['Mustang', 'muscle'],
     image: 'mustang',
 },
 
 {
     id: '4',
     brand: 'honda',
-    model:['Accord'],
+    model:['Accord', 'sedan'],
     image: 'accord',
 },
 
